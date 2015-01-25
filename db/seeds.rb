6181ed7136176783110f8a885436ecad09800e9e
Alex Jarvis <alxjrvs@gmail.com>
2015-01-25 12:14:04 -0500

    The rise of Ragnar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ragnar = Character.create name: "Ragnar"
+StatBlock.create character: ragnar,
+  wisdom_val: 7,
+  intelligence_val: 11,
+  strength_val: 16,
+  constitution_val: 14,
+  dexterity_val: 12,
+  charisma_val: 13
+SkillList.create character: ragnar
+
+klass = Fighter.create
+Level.create character: ragnar, pf_class: klass
+ragnar.race = Human.create
+ragnar.save!
+
+
+
